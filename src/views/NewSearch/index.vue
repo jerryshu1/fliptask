038073b479c55666b22ff9bee1961b4a5aa3d4fa
jerryshu1 <1044787011@qq.com>
2022-10-19 11:18:37 +0800

    xjwchanged_10

--- src/views/NewSearch/index.vue
+++ src/views/NewSearch/index.vue
@@ -10,14 +10,17 @@
         </div>
         <el-steps style="width: 85%; margin-left: 7%; margin-top: 1%" direction="vertical" :active="active">
             <el-step title="请选择公司及站点">
-                <template class="selectgroup" v-slot:description>
-                    <el-select v-model="current_company" class="choosecomp m-2" placeholder="请选择公司" size="mini"
-                        @change="getstationList">
-                        <el-option v-for="item in companylist" :key="item" :label="item" :value="item" />
-                    </el-select>
+                <template v-slot:description>
+                    <div class="selectgroup">
+                        <el-select v-model="current_company" class="choosecomp m-2" placeholder="请选择公司" size="mini"
+                            @change="getstationList">
+                            <el-option v-for="item in companylist" :key="item" :label="item" :value="item" />
+                        </el-select>
 
-                    <el-autocomplete v-model="current_station" :fetch-suggestions="querySearch" clearable
-                        placeholder="请选择站点" @select="handleSelect" />
+                        <el-autocomplete v-model="current_station" :fetch-suggestions="querySearch" clearable
+                            placeholder="请选择站点" @select="handleSelect" />
+                    </div>
+
                 </template>
             </el-step>
             <el-step title="请输入线路名称以及选择间隔单元">
@@ -51,30 +54,29 @@
                             @change="getcommonlist">
                             <el-option v-for="item in statuslist" :key="item" :label="item" :value="item" />
                         </el-select>
-                        
-                        <el-select v-model="current_task" class="ends m-2" placeholder="请选择任务" size="mini" v-if="paths">
-                            <el-option v-for="item in pathlist" :key="item" :label="item"
-                                :value="item" />
+
+                        <el-select v-model="current_tasks" class="ends m-2" placeholder="请选择任务" size="mini" v-if="paths"
+                            @change="gettask">
+                            <el-option v-for="item in pathlist" :key="item" :label="item" :value="item" />
                         </el-select>
                     </div>
                     <div v-if="searchtype === 1">
-                        <el-select v-model="current_task" class="m-2" placeholder="请选择任务" size="mini">
-                            <el-option v-for="(item, index) in othertasklist" :key="item" :label="item.task_name"
+                        <el-select v-model="current_task" class="m-2" placeholder="请选择任务" size="mini"
+                            @change="getcurrentothertask">
+                            <el-option v-for="(item, index) in othertasklist" :key="index" :label="item.task_name"
                                 :value="index" />
                         </el-select>
                     </div>
                 </template>
             </el-step>
-            <el-step title="请选择操作对象、存在的主要风险">
+            <el-step title="请选择操作对象">
                 <template v-slot:description>
                     <el-table ref="taskTableRef" :data="showtabledata[0]" :border="parentBorder" style="width: 100%"
                         @select="handleSelectionChange" :row-key="getRowKeys">
                         <el-table-column type="selection" :reserve-selection="true" />
-                        <el-table-column label="Name" prop="name" />
-                        <el-table-column label="State" prop="state" />
-                        <el-table-column label="City" prop="city" />
-                        <el-table-column label="Address" prop="address" />
-                        <el-table-column label="Zip" prop="zip" />
+                        <el-table-column label="操作设备" prop="device" />
+                        <el-table-column label="设备类型" prop="device_type" />
+                        <el-table-column label="操作方式" prop="operation" />
                     </el-table>
                     <el-button @click="prestep">上一步</el-button>
                     <el-button @click="nextstep">下一步</el-button>
@@ -102,11 +104,13 @@ export default defineComponent({
         const current_category = ref("");
         const current_station = ref("");
         const current_task = ref(null)
+        const current_tasks = ref([])
+        const current_task_use = ref([])
         const looptype = ref("");
         const active = ref(0);
         const searchtype = ref(null);
         const paths = ref(false)
-        const pathlist = ref ([])
+        const pathlist = ref([])
         const start_status = ref("");
         const end_status = ref("");
         // 底下单选的value
@@ -178,6 +182,30 @@ export default defineComponent({
         const current_step = ref(0)
         const multipleSelection = ref([])
         const taskTableRef = ref()
+        const every1 = ref({
+            task_name: '电压互感器',
+            details: [{
+                device: '电压互感器',
+                device_type: '',
+                operation: '停复役'
+            }]
+        })
+        const every2 = ref({
+            task_name: '电流互感器',
+            details: [{
+                device: '电流互感器',
+                device_type: '',
+                operation: '停复役'
+            }]
+        })
+        const every3 = ref({
+            task_name: '避雷器',
+            details: [{
+                device: '避雷器',
+                device_type: '',
+                operation: '复役'
+            }]
+        })
 
         const store = useStore();
 
@@ -235,7 +263,7 @@ export default defineComponent({
             if (val === "线路" || "变压器" || "站用变" || "电抗器" || "电容器") {
                 end_status.value = ''
                 start_status.value = ''
-                paths.value=false
+                paths.value = false
                 searchtype.value = 0;
                 statuslist.value[4] = '开关' + val + '检修'
             }
@@ -265,14 +293,81 @@ export default defineComponent({
             }
             if (val === "线路导母线") {
                 searchtype.value = 2;
+                current_task_use.value = []
+                neededdata.value['paths'] = []
+                neededdata.value['tasks'] = {}
+                neededdata.value['paths'].push('线路倒母线')
+                current_task_use.value.push('线路倒母线')
+                current_task_use.value.push('电压互感器')
+                current_task_use.value.push('电流互感器')
+                current_task_use.value.push('避雷器')
+                Length.value = current_task_use.value.length
+
+                neededdata.value['tasks']['线路倒母线'] = {
+                    task_name: '线路倒母线',
+                    details: [{
+                        device: '断路器',
+                        device_type: 'AIS',
+                        operation: '分合闸'
+                    },{
+                        device: '断路器',
+                        device_type: 'GIS(HGIS)',
+                        operation: '分合闸'
+                    },{
+                        device: '断路器',
+                        device_type: '开关柜',
+                        operation: '分合闸'
+                    },{
+                        device: '隔离开关',
+                        device_type: 'AIS',
+                        operation: '分闸'
+                    },{
+                        device: '隔离开关',
+                        device_type: 'AIS',
+                        operation: '合闸'
+                    },{
+                        device: '隔离开关',
+                        device_type: 'GIS(HGIS)',
+                        operation: '分闸'
+                    },{
+                        device: '隔离开关',
+                        device_type: 'GIS(HGIS)',
+                        operation: '合闸'
+                    },{
+                        device: '线路',
+                        device_type: '双母线',
+                        operation: '倒母操作'
+                    },{
+                        device: '保护压板',
+                        device_type: '硬压板',
+                        operation: '投退'
+                    },{
+                        device: '保护压板',
+                        device_type: '软压板',
+                        operation: '投退'
+                    },]
+                }
+                neededdata.value['tasks']['电压互感器'] = every1.value
+                neededdata.value['tasks']['电流互感器'] = every2.value
+                neededdata.value['tasks']['避雷器'] = every3.value
+
+                showtabledata.value = [neededdata.value['tasks']['线路倒母线'].details]
             }
         };
+        const gettask = (val) => {
+            current_task_use.value = val
+            current_task_use.value.push('电压互感器')
+            current_task_use.value.push('电流互感器')
+            current_task_use.value.push('避雷器')
+            Length.value = current_task_use.value.length
 
+            showtabledata.value = [neededdata.value['tasks'][val[0]].details]
+        }
         const nextstep = () => {
             if (current_step.value + 1 < Length.value) {
                 current_step.value += 1
-                // checkdevice.value.push(value.value)
-                showtabledata.value = [tableData[current_step.value].family]
+                let key = current_task_use.value[current_step.value]
+                showtabledata.value = [neededdata.value['tasks'][key].details]
             } else {
                 // 跳转中间层
             }
@@ -280,7 +375,8 @@ export default defineComponent({
         const prestep = () => {
             if (current_step.value - 1 >= 0) {
                 current_step.value -= 1
-                showtabledata.value = [tableData[current_step.value].family]
+                let key = current_task_use.value[current_step.value]
+                showtabledata.value = [neededdata.value['tasks'][key].details]
             } else {
                 ElMessage.info("已经是第一页！")
             }
@@ -291,23 +387,38 @@ export default defineComponent({
             } else if (start_status.value === '') {
                 ElMessage.error('起始状态未选择')
             } else {
+                let start = ''
+                let end = ''
+                if (start_status.value === statuslist.value[4]) {
+                    start = '开关检修'
+                } else {
+                    start = start_status.value
+                }
+                if (end_status.value === statuslist.value[4]) {
+                    end = '开关检修'
+                } else {
+                    end = end_status.value
+                }
                 let params = {
-                    to: end_status.value,
-                    from: start_status.value
+                    to: end,
+                    from: start
                 }
                 getcomontasklist(current_company_id.value, params).then((res) => {
                     if (res) {
                         if (res.paths === null) {
-                            paths.value=false
+                            paths.value = false
                             ElMessage.error('任务库无此类任务')
                         } else {
                             if (res.paths.length === 1) {
-                                paths.value=false
+                                paths.value = false
                                 neededdata.value = res
                             } else {
                                 paths.value = true
                                 pathlist.value = res.paths
                                 neededdata.value = res
+                                neededdata.value['tasks']['电压互感器'] = every1.value
+                                neededdata.value['tasks']['电流互感器'] = every2.value
+                                neededdata.value['tasks']['避雷器'] = every3.value
                             }
                         }
                     } else {
@@ -318,24 +429,35 @@ export default defineComponent({
         }
 
         const handleSelectionChange = (selection) => {
-            // if (selection.length > 1) {
-            //     let del_row = selection.pop();
-            //     console.log('0',del_row);
-            //     ElMessage("最多只能选1");
-            //     taskTableRef.value.toggleRowSelection(del_row, false)
-            // } else {
-            //     value.value = selection[0];
-            // }
             console.log(selection)
         }
         const getRowKeys = (row) => {
-            return row.name
+            return row.device + row.device_type + row.operation
         }
+
+        const getcurrentothertask = (val) => {
+            let key = othertasklist.value[val]
+
+            neededdata.value['paths'] = []
+            neededdata.value['tasks'] = {}
+            neededdata.value['paths'].push(key.task_name)
+            current_task_use.value = neededdata.value['paths']
+            current_task_use.value.push('电压互感器')
+            current_task_use.value.push('电流互感器')
+            current_task_use.value.push('避雷器')
+            Length.value = current_task_use.value.length
+
+            neededdata.value['tasks'][key.task_name] = key
+            neededdata.value['tasks']['电压互感器'] = every1.value
+            neededdata.value['tasks']['电流互感器'] = every2.value
+            neededdata.value['tasks']['避雷器'] = every3.value
+
+            showtabledata.value = [neededdata.value['tasks'][key.task_name].details]
+        }
+
         onMounted(() => {
             getcompanyList()
             current_company_id.value = store.state.user.company_id
-            Length.value = tableData.length
-            showtabledata.value = [tableData[current_step.value].family]
         })
 
         return {
@@ -360,10 +482,15 @@ export default defineComponent({
             Length,
             taskTableRef,
             current_task,
+            current_tasks,
+            current_task_use,
             othertasklist,
             paths,
             neededdata,
             pathlist,
+            every1,
+            every2,
+            every3,
 
             getcompanyList,
             getstationList,
@@ -375,8 +502,8 @@ export default defineComponent({
             prestep,
             getRowKeys,
             getcommonlist,
-            // selectClick,
-            // selected,
+            gettask,
+            getcurrentothertask,
             handleSelectionChange,
             List,
         };
@@ -390,17 +517,20 @@ export default defineComponent({
 }
 
 .el-select {
+    width: 20%;
     margin-top: 1%;
     margin-bottom: 2%;
 }
 
-.el-autocomplete{
+.el-autocomplete {
+    width: 20%;
     margin-top: 1%;
     margin-bottom: 2%;
 }
 
-.selectgroup{
+.selectgroup {
     display: flex;
+    align-items: center;
 }
 
 .el-radio-group {
@@ -422,6 +552,4 @@ export default defineComponent({
 :deep(.el-table th.el-table__cell:nth-child(1) .cell) {
     visibility: hidden;
 }
-
-
 </style>
